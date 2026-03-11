fba165458b1cbccd87c9f34d64b808ab24e17be4
isha <ishasondagar2@gmail.com>
2026-03-11 17:22:32 +0530

    header desc

---
+++ templates/header.php
@@ -0,0 +1,69 @@
+<header class="header position-fixed top-0 w-100 dpt-30 dpb-30">
+    <div class="container">
+        <div
+            class="d-flex flex-column flex-lg-row justify-content-between align-items-lg-center">
+            <div class="w-100 d-flex justify-content-between align-items-center">
+                <a href="" class="header-logo">
+                    <img
+                        src="assets/images/icon/header-logo.svg"
+                        class="white-logo h-100"
+                        alt="" />
+                    <img
+                        src="assets/images/icon/blck-logo.svg"
+                        class="black-logo h-100"
+                        alt="" />
+                </a>
+                <div
+                    class="burger-menu d-lg-none d-flex align-items-center justify-content-center">
+                    <div class="menu-icon d-inline-flex">
+                        <img
+                            src="assets/images/icon/menu-icon.svg"
+                            class="h-100 w-100 white-icon"
+                            alt="" />
+                        <img
+                            src="assets/images/icon/black-menu.svg"
+                            class="h-100 w-100 black-icon"
+                            alt="" />
+                    </div>
+                    <div class="close-icon">
+                        <img
+                            src="assets/images/icon/close-icon.svg"
+                            class="h-100 w-100"
+                            alt="" />
+                    </div>
+                </div>
+            </div>
+
+            <div
+                class="header-menu d-none d-lg-flex justify-content-between align-items-center tpt-120">
+                <ul class="d-flex flex-column flex-lg-row list-none ps-0 mb-0">
+                    <li class="mx-lg-3 tmb-35">
+                        <a
+                            href=""
+                            class="montserrat-regular font15 leading18 res-font24 res-leading30 text-F0EBE2 fw-normal text-decoration-none text-nowrap text-uppercase">ABOUT</a>
+                    </li>
+                    <li class="mx-lg-3 tmb-35">
+                        <a
+                            href=""
+                            class="montserrat-regular font15 leading18 res-font24 res-leading30 text-F0EBE2 fw-normal text-decoration-none text-nowrap text-uppercase">OUR SERVICES</a>
+                    </li>
+                    <li class="mx-lg-3 tmb-35">
+                        <a
+                            href=""
+                            class="montserrat-regular font15 leading18 res-font24 res-leading30 text-F0EBE2 fw-normal text-decoration-none text-nowrap text-uppercase">CASE STUDIES</a>
+                    </li>
+                    <li class="mx-lg-3 tmb-35">
+                        <a
+                            href=""
+                            class="montserrat-regular font15 leading18 res-font24 res-leading30 text-F0EBE2 fw-normal text-decoration-none text-nowrap text-uppercase">CONTACT</a>
+                    </li>
+                </ul>
+                <div class="ps-lg-4 ms-lg-2 header-btn">
+                    <a
+                        href=""
+                        class="text-decoration-none btnA border-F0EBE2-btn montserrat-regular font15 leading18 text-uppercase rounded-pill d-inline-flex align-items-center transition">CONTACT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
